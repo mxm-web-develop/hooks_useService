98f268d0e83f457a2c142142f246398e4a5f263e
Hanfeng <zhf@33.cn>
2021-11-08 17:14:03 +0800

    update

--- index.ts
+++ index.ts
@@ -5,9 +5,16 @@ interface AuthType{
     password: string;
 }
 
+enum InternetStatus {
+    PENDING,
+    OK,
+    ERROR,
+    TIMEOUT,
+}
+
 interface UserServiceType<D,P>{
-    url: string;
-    method?:Method,
+    url?: string;
+    method?:string,
     baseURL?: string,
     data?:D,
     timeout?:number,
@@ -17,63 +24,72 @@ interface UserServiceType<D,P>{
     auth?:AuthType,
     responseType?:ResponseType,
 }
+
+
 /**
  * @params R-返回的type
  * @params D-发送的参数type
  * @params P-发送的路由params type
  * @returns 
  */
-const useService = <R,D,P>({
-        url,
-        method='get',
-        baseURL,
-        data,
-        timeout=13000,
-        headers,
-        params,
-        withCredentials=false,
-        auth,
-        responseType='json',
-    }:UserServiceType<D,P>):{loading:Ref<boolean>,
-        response:Ref<AxiosResponse<R> | string | undefined>,
+const useService = <R,D,P>(
+    {url,method='get',baseURL='/proxyApi',data,timeout=30000, headers,params,withCredentials=false,auth,responseType='json'}:UserServiceType<D,P>,
+    manually?:boolean,
+    ):{
+        loading:Ref<boolean>,
+        response:Ref<R|undefined>,
         status:Ref<number>,
         uploadProgress:Ref<[number, number] | undefined>,
-        downloadProgress:Ref<[number, number] | undefined>
+        downloadProgress:Ref<[number, number] | undefined>,
+        run:(data?:any)=>Promise<any>
     }=>{
     const loading = ref<boolean>(true);
-    const response = ref<AxiosResponse<R> | string>()
+    const response = ref<R>()
     const uploadProgress = ref<[number,number]>()
     const downloadProgress = ref<[number,number]>()
-    const status = ref<number>(0)
-    axios({
-        method,
-        url,
-        baseURL:baseURL,
-        data,
-        timeout,
-        headers,
-        params,
-        withCredentials,
-        auth,
-        responseType,
-        onUploadProgress:function(p:ProgressEvent){
-            uploadProgress.value = [p.loaded,p.total]
-        },
-        onDownloadProgress:function(p:ProgressEvent){
-            downloadProgress.value = [p.loaded,p.total]
-        }
-    }).then((res)=>{
-        loading.value = false
-        if(res.status===200){
-            status.value = 1
-            response.value = res.data
-        }else{
-            status.value = 2
-            response.value = '请求有问题'
-        }
-    })
+    const status = ref<number>(InternetStatus.PENDING)
+
+
+
+    const run= (data:any)=> {
+        const request = axios.request<R,AxiosResponse<R>,any>({
+            url,
+            method:method as Method,
+            baseURL:baseURL,
+            data,
+            timeout,
+            headers,
+            params,
+            withCredentials,
+            auth,
+            responseType,
+            onUploadProgress:function(p:ProgressEvent){
+                uploadProgress.value = [p.loaded,p.total]
+            },
+            onDownloadProgress:function(p:ProgressEvent){
+                downloadProgress.value = [p.loaded,p.total]
+            }
+        }).then((res)=> {
+            loading.value = false
+            if(res.status===200){
+                status.value = InternetStatus.OK
+                response.value =  res.data
+                return res.data
+            }else{
+                status.value = InternetStatus.ERROR 
+                throw new Error(res.statusText)
+            }
+        }) 
+        console.log('这里是请求函数里面了',data,request);
+        
+        return request
+    }   
+    
+    !manually? run(data):''
+
 
     return{
+        run,
         loading,
         response,
         status,
